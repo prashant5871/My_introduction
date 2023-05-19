e121499cfb63f4f893a2254900e1da974e921ae2
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2023-05-19 14:10:05 +0530

    new message or we can say this is a new commit

--- JS/theme_changer.js
+++ JS/theme_changer.js
@@ -5,13 +5,58 @@ toggleBtn.addEventListener('change', function() {
     change_theme();
   } else {
     // Handle unchecked state if needed
+    chnage_theme_light();
   }
 });
 
 function change_theme() {
-    var x = document.getElementsByClassName('container');
-    for(let i = 0; i < x.length; i++) {
-        x[i].style.color = "white";
-        x[i].style.backgroundColor = "black";
+    var y = document.getElementsByClassName('container');
+    y[0].style.backgroundColor = "black";
+    y[0].style.color = "white";
+    document.querySelector('.container h1').style.color = "white";
+    
+    document.querySelector('.hobby_container').style.backgroundColor = "black";
+    var p = document.getElementsByClassName('section-title');
+    for(let i = 0; i < p.length; i++){
+      p[i].style.backgroundColor = "#383838";
+      p[i].style.color = "white";
+    }
+
+    var a = document.getElementsByClassName('section-content');
+    for(let i = 0; i < a.length; i++){
+      a[i].style.backgroundColor = "#383838";
+      a[i].style.color = "white";
+    }
+
+    var b = document.getElementsByClassName('section-container');
+    for(let i = 0; i < b.length; i++){
+      b[i].style.backgroundColor = "#383838";
+      b[i].style.color = "white";
+    }
+}
+
+function chnage_theme_light(){
+  var y = document.getElementsByClassName('container');
+    y[0].style.backgroundColor = "white";
+    y[0].style.color = "black";
+    document.querySelector('.container h1').style.color = "black";
+    
+    document.querySelector('.hobby_container').style.backgroundColor = "white";
+    var p = document.getElementsByClassName('section-title');
+    for(let i = 0; i < p.length; i++){
+      p[i].style.backgroundColor = "white";
+      p[i].style.color = "black";
+    }
+
+    var a = document.getElementsByClassName('section-content');
+    for(let i = 0; i < a.length; i++){
+      a[i].style.backgroundColor = "white";
+      a[i].style.color = "black";
+    }
+
+    var b = document.getElementsByClassName('section-container');
+    for(let i = 0; i < b.length; i++){
+      b[i].style.backgroundColor = "white";
+      b[i].style.color = "black";
     }
 }
